46d967d5d07bc81251d9f128ca3fb43ac2a2bfb7
ScidaP <patricioscida@gmail.com>
2020-10-10 20:37:00 -0300

    Adding functionalities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-$carouselImg = document.getElementsByClassName("carousel-item");
+if (window.location.toString().match("index.html")) {
+    $carouselImg = document.getElementsByClassName("carousel-item");
 
-function cambiarFondo() {
-    let i = Math.floor(Math.random() * $carouselImg.length); // Creates a random number between the amount of images in carousel, and 0.
-    $carouselImg[i].classList.add("active"); // Adds class "active" to a random carousel image.
+    function cambiarFondo() {
+        let i = Math.floor(Math.random() * $carouselImg.length); // Creates a random number between the amount of images in carousel, and 0.
+        $carouselImg[i].classList.add("active"); // Adds class "active" to a random carousel image.
 }
 
 cambiarFondo();
+}
