6d048b4fd96e00e20441dc04c6b8391dc37bb689
ScidaP <patricioscida@gmail.com>
2020-10-09 22:59:52 -0300

    Fixed a bug in js code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $carouselImg = document.getElementsByClassName("carousel-item");
 
 function cambiarFondo() {
-    let i = Math.floor(Math.random() * $carouselImg.length);
-    $carouselImg[i].classList.add("active");
+    let i = Math.floor(Math.random() * $carouselImg.length); // Creates a random number between the amount of images in carousel, and 0.
+    $carouselImg[i].classList.add("active"); // Adds class "active" to a random carousel image.
 }
 
 cambiarFondo();
