4ab6abf454d4dcf040bd4821ba60583a7d851489
ScidaP <patricioscida@gmail.com>
2020-10-15 20:05:24 -0300

    Adding content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 if (window.location.toString().match("index.html")) {
-    $carouselImg = document.getElementsByClassName("carousel-item");
+    var $carouselImg = document.getElementsByClassName("carousel-item");
 
     function cambiarFondo() {
         let i = Math.floor(Math.random() * $carouselImg.length); // Creates a random number between the amount of images in carousel, and 0.
         $carouselImg[i].classList.add("active"); // Adds class "active" to a random carousel image.
     }
 cambiarFondo();
+}
+
+if (window.location.toString().match("musica.html")) {
+    var $botonRegueton = document.getElementsByClassName("nav-regueton");
+    var $playlistDiv = document.getElementsByClassName("playlist-container");
 }
