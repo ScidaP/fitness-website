5dbb581268373ac8ef5843b5e8d0be3000367ae0
ScidaP <patricioscida@gmail.com>
2020-11-06 23:41:58 -0300

    Added mobile carousel.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if (window.location.toString().match("home")) {
-    var $carouselImg = document.getElementsByClassName("carousel-item");
+    var $carouselImg = document.getElementsByClassName("carousel1-item");
 
     function cambiarFondo() {
         let i = Math.floor(Math.random() * $carouselImg.length); // Creates a random number between the amount of images in carousel, and 0.
