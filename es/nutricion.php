900e5c2799595b8c641f8f9c8efaf2e141a98b49
ScidaP <patricioscida@gmail.com>
2020-11-06 23:36:32 -0300

    Finished website

--- es/nutricion.php
+++ es/nutricion.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/main.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@300;500&display=swap" rel="stylesheet"> 
-    <link rel="shortcut icon" href="../img/favicon.ico">
-    <link rel="stylesheet" href="../css/fontawesomecss/all.min.css">
-    <title>BeFit</title>
+<?php include '../sent/head.php' ?>
+    <title>Nutrición - BeFit</title>
 </head>
 <body>
   <header class="header" id="cielo">
@@ -51,8 +42,8 @@
             <h3 class="mt-5">Con toda esta información uno podría llevar una alimentación adecuada, completa y segura. </h3>
             <p>Ahora bien, en el deporte ya se necesita un plan alimentario específico a cada persona y actividad que se realiza ya sea un deporte o gimnasio. Se deben plantear objetivos a corto plazo y a largo plazo y de ser necesario se puede suplementar el plan de alimentación con aquellos suplementos permitidos o avalados por alguna entidad científica, para estar seguros que consumiremos algo realmente beneficioso. Para esto les traigo lo que es la clasificación de los suplementos por la AIS.</p>
             <div class="container-ais">
-              <img src="../img/ais2.svg" height="400" width="400"> 
-              <img src="../img/ais1.jpg" height="400" width="400">
+              <img src="../img/ais2.svg" height="400" width="400" class="img-fluid"> 
+              <img src="../img/ais1.jpg" height="400" width="400" class="img-fluid">
             </div>
             <p class="mt-3">En cuanto a la suplementación en el deporte el Instituto Australiano del Deporte o Australian Institute of Sport (AIS), una institución deportiva de referencia encargada de asegurar que los deportistas australianos de alto rendimiento dispongan de fondos y prestaciones para la actividad deportiva, creó el <a href="../downloadable/AIS-Sports-Supplement-Framework-2019.pdf" download>Sistema de Clasificación ABCD</a>, un método clasificatorio que categoriza los suplementos nutricionales según su evidencia científica, además de otras consideraciones prácticas que determinan si son seguros, legales y eficaces para mejorar el rendimiento deportivo. La AIS es, junto a la ISSN (sociedad Internacional de Nutrición Deportiva), dos de las grandes instituciones de nutrición deportiva de referencia. A pesar de que ambas son absolutas referencias, a nivel de investigación, la clasificación del AIS es la más utilizada para categorizar los suplementos.</p>
             <div class="parallax mb-4 mt-4"></div>
@@ -114,8 +105,4 @@
       <?php include '../sent/cielo.php' ?>
       <?php include '../sent/footer.php'?>
 </body>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-<script src="../src/index.js"></script>
-</html>
+<?php include '../sent/scripts.php' ?>
--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" class="home-html">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/main.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@300;500&display=swap" rel="stylesheet"> 
     <link rel="shortcut icon" href="img/favicon.ico">
-    <title id="title">BeFit</title>
+    <title id="title">Home - BeFit</title>
 </head>
 <body>
   <header>
@@ -47,59 +47,59 @@
           </ul>
         </div>
       </nav>
-      <main>
+      <main class="main-home">
         <div id="carousel1" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio1.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">¡Nunca te rindas! El fracaso y el rechazo son solo el primer paso para tener éxito.</p>
                 <footer class="blockquote-footer user-select-none">Jim Valvano</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/jordan.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">Tienes que esperar cosas de ti mismo antes de poder hacerlas.</p>
                 <footer class="blockquote-footer user-select-none">Michael Jordan</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio2.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">La edad no es una barrera. Es una limitación que pones en tu mente.</p>
                 <footer class="blockquote-footer user-select-none">Jackie Joyner-Keerse</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio3.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">No te midas por lo que has logrado, sino por lo que deberías haber logrado con tu habilidad.</p>
                 <footer class="blockquote-footer user-select-none">John Wooden</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio5.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">Todo depende del viaje, no del resultado.</p>
                 <footer class="blockquote-footer user-select-none">Carl Lewis</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio6.jpeg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">Si entrenas duro, no solo serás duro, serás duro de vencer.</p>
                 <footer class="blockquote-footer user-select-none">Herschel Walker</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio7.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">Solo hay dos opciones con respecto al compromiso. Estás dentro o estás fuera. No existe tal cosa como la vida en el medio.</p>
                 <footer class="blockquote-footer user-select-none">Pat Riley</footer>
               </blockquote>
             </div>
-            <div class="carousel-item image-container">
+            <div class="carousel-item carousel1-item image-container">
               <img src="img/inicio8.jpg" class="d-block w-100">
               <blockquote class="blockquote middle-left">
                 <p class="mb-0 sombreado user-select-none">Si le tienes miedo al fracaso, no mereces tener éxito.</p>
@@ -112,6 +112,53 @@
             <span class="sr-only">Previous</span>
           </a>
           <a class="carousel-control-next" href="#carousel1" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <div id="carousel2" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item image-container active">
+              <img src="img/iniciomobile1.jpg" class="d-block">
+              <blockquote class="blockquote middle-left">
+                <p class="mb-0 sombreado user-select-none">Si le tienes miedo al fracaso, no mereces tener éxito.</p>
+                <footer class="blockquote-footer user-select-none">Charles Barkley</footer>
+              </blockquote>
+            </div>
+            <div class="carousel-item image-container">
+              <img src="img/iniciomobile2.jpg" class="d-block">
+              <blockquote class="blockquote middle-left">
+                <p class="mb-0 sombreado user-select-none">Si entrenas duro, no solo serás duro, serás duro de vencer.</p>
+                <footer class="blockquote-footer user-select-none">Herschel Walker</footer>
+              </blockquote>
+            </div>
+            <div class="carousel-item image-container">
+              <img src="img/iniciomobile3.jpg" class="d-block">
+              <blockquote class="blockquote middle-left">
+                <p class="mb-0 sombreado user-select-none">Todo depende del viaje, no del resultado.</p>
+                <footer class="blockquote-footer user-select-none">Carl Lewis</footer>
+              </blockquote>
+            </div>
+            <div class="carousel-item image-container">
+              <img src="img/iniciomobile4.jpg" class="d-block">
+              <blockquote class="blockquote middle-left">
+                <p class="mb-0 sombreado user-select-none">La edad no es una barrera. Es una limitación que pones en tu mente.</p>
+                <footer class="blockquote-footer user-select-none">Jackie Joyner-Keerse</footer>
+              </blockquote>
+            </div>
+            <div class="carousel-item image-container">
+              <img src="img/iniciomobile5.jpg" class="d-block">
+              <blockquote class="blockquote middle-left">
+                <p class="mb-0 sombreado user-select-none">Tienes que esperar cosas de ti mismo antes de poder hacerlas.</p>
+                <footer class="blockquote-footer user-select-none">Michael Jordan</footer>
+              </blockquote>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carousel2" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carousel2" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
